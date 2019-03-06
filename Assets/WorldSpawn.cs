55b0b369cd2987b24c7ba9f87bf4d3c52bcb4101
Balder <balderbrusch@gmail.com>
2019-03-06 13:08:42 +0100

    world spawner
    world spawn system done

---
+++ Assets/WorldSpawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldSpawn : MonoBehaviour
+{
+ public GameObject[] WorldMonuments;
+
+
+    private Vector3 placeToSpawn;    
+
+
+	// Use this for initialization
+	void Start () {
+
+    for(int i = 0; i < WorldMonuments.Length; i++){
+      
+        placeToSpawn = new Vector3(Random.Range(-500, 500), 0, Random.Range(-500, 500));
+	Instantiate(WorldMonuments[i], placeToSpawn, Quaternion.identity);
+        
+        }
+	}
+}
---
+++ Assets/worldSpawnCollisionPreventer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class worldSpawnCollisionPreventer : MonoBehaviour
+{
+    
+     void OnTriggerStay(Collider target)
+     {
+         if(target.tag == "WorldMonument")
+         {
+            transform.position=new Vector3(Random.Range(-2000, 2000),0,Random.Range(-2000, 2000));
+            Debug.Log("monuments moved!");
+         }
+     }
+
+
+}
