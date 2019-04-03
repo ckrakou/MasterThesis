457a5a1be30dc6d01e25edf5a17908d448ee9169
Balder <balderbrusch@gmail.com>
2019-04-03 10:18:11 +0200

    cartoon tree prefab and monstercam

---
+++ Assets/Scripts/worldSpawnCollisionPreventerMonsterCam.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class worldSpawnCollisionPreventerMonsterCam : MonoBehaviour
+{
+
+    public GameObject Light; 
+    public  GameObject Particles; 
+
+    public GameObject WorldMonument;
+
+    public AudioSource GlitchSound;
+    public AudioSource Speak;
+
+    public GameObject MonsterCam;
+
+    public GameObject PlayerCam;
+
+    private bool OnlyBlinkOnce = true;
+
+    private bool MonsterCamSwitch = false;
+
+
+
+
+    void Awake(){
+     
+    }
+
+  
+  /*
+    void Update(){
+
+          if(MonsterCamSwitch){
+               
+                      PlayerCam.SetActive(false);
+                       MonsterCam.SetActive(true);
+                       MonsterCamSwitch=false;
+                
+          }
+    }
+    */
+    
+     void OnTriggerStay(Collider target)
+     {
+         if(target.tag == "WorldMonument")
+         {
+          //  transform.position=new Vector3(Random.Range(-100, 100),0,Random.Range(-100, 100));
+            //Debug.Log("monuments moved!");
+         }
+     }
+      void OnTriggerEnter(Collider target)
+     {
+         if(target.tag == "Player")
+         {
+          
+             if(OnlyBlinkOnce){
+             StartCoroutine(Blink());
+             OnlyBlinkOnce=false;
+         //      MonsterCamSwitch=true;
+             }
+         }
+     }
+
+       IEnumerator Blink() {
+
+       WorldMonument.GetComponent<Renderer>().enabled=false;
+         Particles.GetComponent<Renderer>().enabled=false;
+    yield return new WaitForSeconds(Random.Range(0.05f,0.5f));       
+ 
+   WorldMonument.GetComponent<Renderer>().enabled=true;
+         Particles.GetComponent<Renderer>().enabled=true;
+         GlitchSound.pitch = Random.Range(0.9f,1.9f);
+   GlitchSound.Play();
+    yield return new WaitForSeconds(Random.Range(0.05f,0.5f));       
+
+      WorldMonument.GetComponent<Renderer>().enabled=false;
+         Particles.GetComponent<Renderer>().enabled=false;
+          GlitchSound.Pause();
+    yield return new WaitForSeconds(Random.Range(0.05f,0.5f));       
+ 
+   WorldMonument.GetComponent<Renderer>().enabled=true;
+         Particles.GetComponent<Renderer>().enabled=true;
+         GlitchSound.pitch = Random.Range(0.9f,1.9f);
+   GlitchSound.Play();
+    yield return new WaitForSeconds(Random.Range(0.05f,0.5f));  
+
+       WorldMonument.GetComponent<Renderer>().enabled=false;
+         Particles.GetComponent<Renderer>().enabled=false;
+         GlitchSound.Pause();
+    yield return new WaitForSeconds(Random.Range(0.05f,0.5f));       
+ 
+   WorldMonument.GetComponent<Renderer>().enabled=true;
+         Particles.GetComponent<Renderer>().enabled=true;
+         GlitchSound.pitch = Random.Range(0.9f,1.9f);
+   GlitchSound.Play();
+    yield return new WaitForSeconds(Random.Range(0.05f,0.5f));  
+
+       WorldMonument.GetComponent<Renderer>().enabled=false;
+         Particles.GetComponent<Renderer>().enabled=false;
+         GlitchSound.Pause();
+    yield return new WaitForSeconds(Random.Range(0.05f,0.5f));       
+ 
+   WorldMonument.GetComponent<Renderer>().enabled=true;
+         Particles.GetComponent<Renderer>().enabled=true;
+         GlitchSound.pitch = Random.Range(0.9f,1.9f);
+   GlitchSound.Play();
+    yield return new WaitForSeconds(Random.Range(0.05f,0.5f));  
+
+       WorldMonument.GetComponent<Renderer>().enabled=false;
+         Particles.GetComponent<Renderer>().enabled=false;
+         GlitchSound.Pause();
+    yield return new WaitForSeconds(Random.Range(0.05f,0.5f));       
+ 
+   WorldMonument.GetComponent<Renderer>().enabled=true;
+         Particles.GetComponent<Renderer>().enabled=true;
+         GlitchSound.pitch = Random.Range(0.9f,1.9f);
+    GlitchSound.Play();
+  
+
+    yield return new WaitForSeconds(Random.Range(0.05f,0.5f));      
+   GlitchSound.Stop();
+      Speak.Play();
+     Destroy(Light);
+    Destroy(Particles);    
+  
+ }
+
+
+}
