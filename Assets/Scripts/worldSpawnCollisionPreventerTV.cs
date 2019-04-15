400c6a87e49e3158afa6007dfc1d6270e4060c06
Christoffer - from home <chris.krakou@gmail.com>
2019-04-15 12:55:59 +0200

    Video play in webgl build now works

--- Assets/Scripts/worldSpawnCollisionPreventerTV.cs
+++ Assets/Scripts/worldSpawnCollisionPreventerTV.cs
@@ -6,8 +6,8 @@ using UnityEngine.Video;
 public class worldSpawnCollisionPreventerTV : MonoBehaviour
 {
 
-    public GameObject Light; 
-    public  GameObject Particles; 
+    public GameObject Light;
+    public GameObject Particles;
 
     public GameObject WorldMonument;
 
@@ -18,53 +18,58 @@ public class worldSpawnCollisionPreventerTV : MonoBehaviour
 
 
 
-    void Start(){
-     video.Prepare();
+    void Start()
+    {
+        video.Prepare();
     }
-    
-     void OnTriggerStay(Collider target)
-     {
-         if(target.tag == "WorldMonument")
-         {
+
+    void OnTriggerStay(Collider target)
+    {
+        if (target.tag == "WorldMonument")
+        {
             //transform.position=new Vector3(Random.Range(-100, 100),0,Random.Range(-100, 100));
             //Debug.Log("monuments moved!");
-         }
-     }
-      void OnTriggerEnter(Collider target)
-     {
-         if(target.tag == "Player")
-         {
-          
-             if(OnlyBlinkOnce){
-             StartCoroutine(Blink());
-             OnlyBlinkOnce=false;
-             }
-         }
-     }
+        }
+    }
+    void OnTriggerEnter(Collider target)
+    {
+        if (target.tag == "Player")
+        {
 
-       IEnumerator Blink() {
+            if (OnlyBlinkOnce)
+            {
+                StartCoroutine(Blink());
+                OnlyBlinkOnce = false;
+            }
+        }
+    }
+
+    IEnumerator Blink()
+    {
 
 
-         for(int i = 0; i < 11; i++){         
-       WorldMonument.GetComponent<Renderer>().enabled=false;
-         Particles.GetComponent<Renderer>().enabled=false;
-    yield return new WaitForSeconds(Random.Range(0.05f,0.5f));       
- 
-   WorldMonument.GetComponent<Renderer>().enabled=true;
-         Particles.GetComponent<Renderer>().enabled=true;
-         GlitchSound.pitch = Random.Range(0.9f,1.9f);
-   GlitchSound.Play();
-   yield return new WaitForSeconds(Random.Range(0.05f,0.5f));   
-         }
-         
-   GlitchSound.Stop();
-      video.Play();
-     Destroy(Light);
-    Destroy(Particles);
-     
-    
-      
- }
+        for (int i = 0; i < 11; i++)
+        {
+            WorldMonument.GetComponent<Renderer>().enabled = false;
+            Particles.GetComponent<Renderer>().enabled = false;
+            yield return new WaitForSeconds(Random.Range(0.05f, 0.5f));
+
+            WorldMonument.GetComponent<Renderer>().enabled = true;
+            Particles.GetComponent<Renderer>().enabled = true;
+            GlitchSound.pitch = Random.Range(0.9f, 1.9f);
+            GlitchSound.Play();
+            yield return new WaitForSeconds(Random.Range(0.05f, 0.5f));
+        }
+
+        GlitchSound.Stop();
+        video.url = System.IO.Path.Combine(Application.streamingAssetsPath, "TVShows_2.webm");
+        video.Play();
+        Destroy(Light);
+        Destroy(Particles);
+
+
+
+    }
 
 
 }
