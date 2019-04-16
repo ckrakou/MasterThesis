677c72f5d01cf4df628aa5733728128ef09fc472
Christoffer - from home <chris.krakou@gmail.com>
2019-04-16 16:41:29 +0200

    minor adjustment on video

--- Assets/Scripts/videoEye.cs
+++ Assets/Scripts/videoEye.cs
@@ -9,36 +9,42 @@ public class videoEye : MonoBehaviour
     private VideoPlayer video;
     public GameObject[] VideoObjects;
 
-     private int RandomVideo;
-     private bool DestroyEverything = false;
+    private int RandomVideo;
+    private bool DestroyEverything = false;
 
     public UnityEvent VideoEyeEvents;
     // Start is called before the first frame update
     void Awake()
     {
-       
-        RandomVideo=Random.Range(0,7);
+
+        RandomVideo = Random.Range(0, 7);
         video = VideoObjects[RandomVideo].GetComponent<VideoPlayer>();
-        for(int i=0; i<7;i++){
-         VideoObjects[i].SetActive(false);
+        for (int i = 0; i < 7; i++)
+        {
+            VideoObjects[i].GetComponent<VideoPlayer>().url = System.IO.Path.Combine(Application.streamingAssetsPath, i+1 + ".mp4");
+            VideoObjects[i].SetActive(false);
         }
     }
 
-void Update(){
-    if(DestroyEverything){
-        if(!video.isPlaying){
-        Destroy(gameObject,0.1f);
+    void Update()
+    {
+        if (DestroyEverything)
+        {
+            if (!video.isPlaying)
+            {
+                Destroy(gameObject, 0.1f);
+            }
         }
     }
-}
 
-    public void PlayEyeVideo(){
+    public void PlayEyeVideo()
+    {
         VideoObjects[RandomVideo].SetActive(true);
         VideoEyeEvents.Invoke();
         DestroyEverything = true;
 
-       
-     
+
+
     }
-   
+
 }
